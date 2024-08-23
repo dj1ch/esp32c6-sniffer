306f36d16555249ac22f09a78c4455d9d4a23edd
dj1ch <tg.1ch0712@gmail.com>
2024-08-22 23:29:59 -0700

    it won't stop but it works!!!

--- main/main.c
+++ main/main.c
@@ -37,12 +37,7 @@
 #include "esp_console.h"
 #include "esp_vfs_fat.h"
 #include "esp_task_wdt.h"
-
-// drivers
-#include "driver/uart_vfs.h"
-#include "driver/uart.h"
-#include "driver/usb_serial_jtag.h"
-#include "driver/usb_serial_jtag_vfs.h"
+#include "esp_vfs_dev.h"
 
 // other CLI related libraries
 #include "linenoise/linenoise.h"
@@ -72,7 +67,6 @@
 
 // define terminal thingy
 #define PROMPT_STRING "esp32c6"
-static const char* TAG = "example";
 
 #define WDT_TIMEOUT 3 // 3 second timeout
 
@@ -93,7 +87,6 @@ void nvs_init(void);
 
 // CLI
 void cli_init(void);
-void cli_loop(void *pvParameters);
 
 // watchdog
 void wdt_reset(void *pvParameter);
@@ -143,151 +136,46 @@ void nvs_init(void)
  */
 void cli_init(void)
 {
-    // flush stdout
-    fflush(stdout);
-    fsync(fileno(stdout));
-
-    // disable buffering
-    setvbuf(stdin, NULL, _IONBF, 0);
-
-    usb_serial_jtag_vfs_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
-    usb_serial_jtag_vfs_set_rx_line_endings(ESP_LINE_ENDINGS_CRLF);
-    usb_serial_jtag_driver_config_t cfg = USB_SERIAL_JTAG_DRIVER_CONFIG_DEFAULT();
-    usb_serial_jtag_driver_install(&cfg);
-
-    usb_serial_jtag_vfs_register();
-
-    // initialize console
-    esp_console_config_t console_config = {
-        .max_cmdline_args = 8,
-        .max_cmdline_length = 256,
-    #if CONFIG_LOG_COLORS
-        .hint_color = atoi(LOG_COLOR_CYAN)
-    #endif
-    };
-    ESP_ERROR_CHECK( esp_console_init(&console_config) );
-
-    // configure linenoise
-    linenoiseSetMultiLine(1);
-
-    // when to do line completion
-    linenoiseSetCompletionCallback(&esp_console_get_completion);
-    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
-
-    // max history size
-    linenoiseHistorySetMaxLen(100);
-
-    // max command length
-    linenoiseSetMaxLineLen(console_config.max_cmdline_length);
-
-    // no empty lines
-    linenoiseAllowEmpty(false);
-
+    // init NVS and fs if needed
+    nvs_init();
     #if CONFIG_STORE_HISTORY
-        // load command history
-        linenoiseHistoryLoad(HISTORY_PATH);
+    fs_init();
     #endif
 
-    ESP_LOGI(TAG, "CLI initialized successfully");
+    // configure REPL
+    esp_console_repl_t *repl = NULL;
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    repl_config.prompt = PROMPT_STRING "> ";
+    repl_config.max_cmdline_length = 256;
+
+    // commands
+    esp_console_register_help_command();
+    register_system_common();
+    #if SOC_WIFI_SUPPORTED
+    register_wifi();
+    #endif
+    register_nvs();
+
+    // initialize repl
+    #if defined(CONFIG_ESP_CONSOLE_UART_DEFAULT) || defined(CONFIG_ESP_CONSOLE_UART_CUSTOM)
+    esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
+
+    #elif defined(CONFIG_ESP_CONSOLE_USB_CDC)
+    esp_console_dev_usb_cdc_config_t hw_config = ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_cdc(&hw_config, &repl_config, &repl));
+
+    #elif defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
+    esp_console_dev_usb_serial_jtag_config_t hw_config = ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl));
+    ESP_ERROR_CHECK(esp_console_start_repl(repl));
+
+    #else
+    #error Unsupported console type
+    #endif
+
 }
 
-void cli_loop(void *pvParameters)
-{
-    while (1) {
-        // initialize stuff
-        nvs_init();
-
-        #if CONFIG_STORE_HISTORY
-            fs_init();
-            ESP_LOGI(TAG, "Command history enabled");
-        #else
-            ESP_LOGI(TAG, "Command history disabled");
-        #endif
-
-        cli_init();
-
-        // registering
-        esp_console_register_help_command();
-        register_system_common();
-        #if SOC_LIGHT_SLEEP_SUPPORTED
-            register_system_light_sleep();
-        #endif
-        #if SOC_DEEP_SLEEP_SUPPORTED
-            register_system_deep_sleep();
-        #endif
-        #if SOC_WIFI_SUPPORTED
-            register_wifi();
-        #endif
-            register_nvs();
-
-        // prompt before each line
-        const char* prompt = LOG_COLOR_I PROMPT_STRING "> " LOG_RESET_COLOR;
-
-        printf("\n"
-           "Type 'help' to get the list of commands.\n"
-           "Use UP/DOWN arrows to navigate through command history.\n"
-           "Press TAB when typing command name to auto-complete.\n"
-           "Press Enter or Ctrl+C will terminate the console environment.\n");
-
-        /* Figure out if the terminal supports escape sequences */
-        int probe_status = linenoiseProbe();
-        if (probe_status) { /* zero indicates success */
-            printf("\n"
-               "Your terminal application does not support escape sequences.\n"
-               "Line editing and history features are disabled.\n"
-               "On Windows, try using Putty instead.\n");
-            linenoiseSetDumbMode(1);
-        #if CONFIG_LOG_COLORS
-            /* Since the terminal doesn't support escape sequences,
-            * don't use color codes in the prompt.
-            */
-            prompt = PROMPT_STRING "> ";
-        #endif //CONFIG_LOG_COLORS
-        }
-
-        // main loop
-        while(1) {
-            // reset watchdog!!!
-            esp_task_wdt_reset();
-            
-            // get a line using linenoise
-            char* line = linenoise(prompt);
-            if (line == NULL) { // break on EOF or error
-                printf("Line input is null");
-                break;
-            }
-            // add line to history
-            if (strlen(line) > 0) {
-                linenoiseHistoryAdd(line);
-            #if CONFIG_STORE_HISTORY
-                // save history to fs
-                linenoiseHistorySave(HISTORY_PATH);
-            #endif
-                vTaskDelay(pdMS_TO_TICKS(10));
-            }
-
-            // run the command
-            int ret;
-            esp_err_t err = esp_console_run(line, &ret);
-            if (err == ESP_ERR_NOT_FOUND) {
-                printf("Unrecognized command\n");
-            } else if (err == ESP_ERR_INVALID_ARG) {
-            // command was empty
-            } else if (err == ESP_OK && ret != ESP_OK) {
-                printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
-            } else if (err != ESP_OK) {
-                printf("Internal error: %s\n", esp_err_to_name(err));
-            }
-                // free the heap
-                linenoiseFree(line);
-                vTaskDelay(pdMS_TO_TICKS(100));
-            }
-
-        ESP_LOGE(TAG, "Error or end-of-input, terminating console");
-        esp_console_deinit();
-        vTaskDelete(NULL);
-    }
-}
 
 void app_main(void)
 {
@@ -306,5 +194,7 @@ void app_main(void)
     }
 
     // create task
-    xTaskCreatePinnedToCore(cli_loop, "CLI Task", 2048, NULL, 1, &cli_task, 0);
+    // xTaskCreatePinnedToCore(cli_init, "CLI Task", 2048, NULL, 1, &cli_task, 0);
+
+    cli_init();
 }
